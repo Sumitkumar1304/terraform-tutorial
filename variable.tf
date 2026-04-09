e51a8dfbd9c97a8f7992701faa51018c30749107
sumit kuamr <badsjsumitkumar@gmail.com>
2026-04-09 23:33:54 +0530

    set default value of user

--- variable.tf
+++ variable.tf
@@ -1,4 +1,13 @@
-variable "user" {}
+/* variable "user" {}
 output "user" {
+    value = var.user
+} */
+
+variable "user" {
+    default = "sumit"
+  
+}
+
+output "name" {
     value = var.user
 }
